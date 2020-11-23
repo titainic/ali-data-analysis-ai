3af0c08c62db72c318e16b0cf6b9416d3c912785
titanic <binend@live.cn>
2020-11-23 17:30:22 +0800

    全表数据类型String转换double

--- java-ai/src/main/java/ali/data/explore/RidgeRegressionExample.java
+++ java-ai/src/main/java/ali/data/explore/RidgeRegressionExample.java
@@ -1,9 +1,21 @@
 package ali.data.explore;
 
+
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
+import org.apache.spark.sql.types.*;
+
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
 
 /**
  * L2岭回归示例
@@ -24,19 +36,50 @@ public class RidgeRegressionExample
                 appName("Correlation").
                 getOrCreate();
 
+        spark.sparkContext().addJar("target/java-ai-1.0-SNAPSHOT.jar");
+
         Dataset<Row> trainDS = spark.read()
              .option("header", "true")
              .option("delimiter","\t")
              .format("csv")
              .load("src/main/resources/zhengqi_test.txt");
 
-        trainDS.show();
+        final String[] columns = trainDS.columns();
+        StructField[] lrTrainFieidArray = new StructField[columns.length];
 
-        LinearRegression lr = new LinearRegression();
-        /**
-         * 设置ElasticNet混合参数。当= 0时，惩罚是L2。等于= 1，它是一个L1。等于 0 < alpha < 1，惩罚是L1和L2的组合。默认值0.0是L2惩罚。
-         */
-        lr.setElasticNetParam(0.0);
+        for (int i = 0; i < columns.length; i++)
+        {
+            StructField structField = new StructField(columns[i], DataTypes.DoubleType, true, Metadata.empty());
+            lrTrainFieidArray[i] = structField;
+        }
+
+        StructType lrTrainDsSchema = new StructType(lrTrainFieidArray);
+
+        Dataset<Row> lrTrainDS = trainDS.map(new MapFunction<Row, Row>()
+        {
+            public Row call(Row row) throws Exception
+            {
+                List<Double> list = new ArrayList<Double>();
+                for (int i = 0; i < columns.length; i++)
+                {
+                   double data =  Double.parseDouble(row.getString(i));
+                   list.add(data);
+                }
+                Row rowx = RowFactory.create(list.toArray());
+                return rowx;
+            }
+        }, RowEncoder.apply(lrTrainDsSchema));
+
+        trainDS.printSchema();
+        System.out.println("------ 转换数据类型，string -> double -----");
+        lrTrainDS.printSchema();
+
+
+//        LinearRegression lr = new LinearRegression();
+//        /**
+//         * 设置ElasticNet混合参数。当= 0时，惩罚是L2。等于= 1，它是一个L1。等于 0 < alpha < 1，惩罚是L1和L2的组合。默认值0.0是L2惩罚。
+//         */
+//        lr.setElasticNetParam(0.0);
 
         spark.stop();
 
